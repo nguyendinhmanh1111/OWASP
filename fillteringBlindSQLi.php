71b7163e1cd223ad7bf110fb406096898e5926e5
nguyendinhmanh1111 <76999751+nguyendinhmanh1111@users.noreply.github.com>
2021-06-30 20:44:28 +0700

    Add files via upload

---
+++ fillteringBlindSQLi.php
@@ -0,0 +1,29 @@
+<?php
+
+if( isset( $_GET[ 'Submit' ] ) ) {
+    // Get input
+    $id = $_GET[ 'id' ];
+
+    // Was a number entered?
+    if(is_numeric( $id )) {
+        // Check the database
+        $data = $db->prepare( 'SELECT first_name, last_name FROM users WHERE user_id = (:id) LIMIT 1;' );
+        $data->bindParam( ':id', $id, PDO::PARAM_INT );
+        $data->execute();
+
+        // Get results
+        if( $data->rowCount() == 1 ) {
+            // Feedback for end user
+            echo '<pre>User ID exists in the database.</pre>';
+        }
+        else {
+            // User wasn't found, so the page wasn't!
+            header( $_SERVER[ 'SERVER_PROTOCOL' ] . ' 404 Not Found' );
+
+            // Feedback for end user
+            echo '<pre>User ID is MISSING from the database.</pre>';
+        }
+    }
+}
+
+?> 
